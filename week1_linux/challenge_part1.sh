51286b89382f044f83b1c097e7cab6462e4c734c
pablo.castillo <pablo.castillo@codein.cloud>
2024-07-14 20:54:34 -0600

    "Challenge part 2"

--- week1_linux/challenge_part1.sh
+++ week1_linux/challenge_part1.sh
@@ -1,20 +1,20 @@
-# Crear un nuevo usuario llamado devuser
+# Create devuser
 sudo useradd -m -s /bin/bash devuser
 
-# Asignar una contraseña segura a devuser
+# Create devuser password
 sudo passwd devuser
 
-# Crear un grupo llamado developers
+# Create developers group
 sudo groupadd developers
 
-# Agregar devuser al grupo developers
+# Add devuser to  developers group
 sudo usermod -aG developers devuser
 
-# Crear un directorio llamado /opt/devproject
+# Create directory /opt/devproject
 sudo mkdir -p /opt/devproject
 
-# Cambiar la propiedad del directorio para que pertenezca a devuser y al grupo developers
+# Change properties
 sudo chown devuser:developers /opt/devproject
 
-# Asignar permisos de lectura, escritura y ejecución al propietario y al grupo, pero solo permisos de lectura y ejecución para otros usuarios
+# Assign properties
 sudo chmod 770 /opt/devproject
